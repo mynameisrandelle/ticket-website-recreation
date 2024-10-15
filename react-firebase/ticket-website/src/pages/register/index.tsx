a1bbccf383f9dea6cfd2a39452b2e88c38942510
mynameisrandelle <kirito123itona@gmail.com>
2024-10-15 22:11:49 +0800

    Setup the pages and routers for the website

---
+++ react-firebase/ticket-website/src/pages/register/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+
+interface IRegisterProps {}
+
+const Register: React.FunctionComponent<IRegisterProps> = (props) => {
+    return <div>Register</div>;
+}
+
+export default Register;
