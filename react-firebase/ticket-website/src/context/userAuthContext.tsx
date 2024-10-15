76802bcfe3fda32bac86287cd515c50349d29f9c
mynameisrandelle <kirito123itona@gmail.com>
2024-10-16 01:59:55 +0800

    Setup the User Auth Context Api
    in addition, i set the isAuth = false to return to login page if not authorized account

---
+++ react-firebase/ticket-website/src/context/userAuthContext.tsx
@@ -0,0 +1,73 @@
+import { auth } from "@/firebaseConfig";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, User } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+
+interface IUserAuthProviderProps {
+    children: React.ReactNode;
+}
+
+
+
+type AuthContextData = {
+    user: User | null;
+    logIn: typeof logIn;
+    signUp: typeof signUp;
+    logOut: typeof logOut;
+    googleSignIn: typeof googleSignIn;
+}
+
+const logIn = (email: string, password: string) => {
+    return signInWithEmailAndPassword(auth, email, password);
+}
+
+const signUp = (email: string, password: string) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+}
+
+const logOut = () => {
+     signOut(auth);
+}
+
+const googleSignIn = () => {
+    const googleAuthProvider = new GoogleAuthProvider;
+    return signInWithPopup(auth, googleAuthProvider);
+}
+
+export const userAuthContext = createContext<AuthContextData> ({
+    user: null,
+    logIn,
+    signUp,
+    logOut,
+    googleSignIn,
+})
+
+export const UserAuthProvider: React.FunctionComponent<IUserAuthProviderProps> = ({children}) => {
+    const [user, setUser] = useState<User | null> (null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                console.log( "testing the watersss", user);
+                setUser(user);
+            }
+
+            return () => {
+                unsubscribe();
+            };
+
+        });
+    });
+
+    const value: AuthContextData = {
+        user,
+        logIn,
+        signUp,
+        logOut,
+        googleSignIn,
+    };
+
+
+    return (
+        <userAuthContext.Provider value={value}>{children}</userAuthContext.Provider>
+    );
+};
