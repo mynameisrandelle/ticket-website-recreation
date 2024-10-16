d13c424573d2a6b1533d825cfd15d63c6067478d
mynameisrandelle <kirito123itona@gmail.com>
2024-10-16 11:21:40 +0800

    functional login and registration system
    made it possible to create account and login using google email with firebase db

--- react-firebase/ticket-website/src/App.tsx
+++ react-firebase/ticket-website/src/App.tsx
@@ -1,12 +1,16 @@
-import * as React from 'react';
-import { RouterProvider } from 'react-router-dom';
-import router from './routes';
-
+import * as React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+import { UserAuthProvider } from "./context/userAuthContext";
 
 interface IAppProps {}
 
 const App: React.FunctionComponent<IAppProps> = (props) => {
-    return <RouterProvider router={router}/>;
-}
+  return (
+    <UserAuthProvider>
+      <RouterProvider router={router} />
+    </UserAuthProvider>
+  );
+};
 
 export default App;
