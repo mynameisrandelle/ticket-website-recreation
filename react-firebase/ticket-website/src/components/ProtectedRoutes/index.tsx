016db3d2f2406555adff03e9220a326523dea867
mynameisrandelle <kirito123itona@gmail.com>
2024-10-16 14:21:30 +0800

    Installed react-firebase-hook
    this allows to save the email and login to remain when refreshed

--- react-firebase/ticket-website/src/components/ProtectedRoutes/index.tsx
+++ react-firebase/ticket-website/src/components/ProtectedRoutes/index.tsx
@@ -1,14 +1,19 @@
 import { useUserAuth } from '@/context/userAuthContext';
 import * as React from 'react';
-import { Navigate, Outlet, useLocation } from 'react-router-dom'
-
-
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { useAuthState} from "react-firebase-hooks/auth";
+import { getAuth } from 'firebase/auth';
 
 interface IProtectedRoutesProps {}
 
 const ProtectedRoutes: React.FunctionComponent<IProtectedRoutesProps> = (props) => {
-    const {user} = useUserAuth();
+    const auth = getAuth();
+    const [user, loading] = useAuthState(auth);
     const location = useLocation();
+
+    if (loading) {
+        return <div>... Loading</div>;
+    }
 
     return user ? (<Outlet />) : (
         <Navigate to="/login" state={{ from: location}} />
